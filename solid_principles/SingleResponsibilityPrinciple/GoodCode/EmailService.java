b8addaa96106d56df7badcc5a6f32695e6fa8f2e
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-12 22:11:05 +0530

    single responsibility principle

---
+++ solid_principles/SingleResponsibilityPrinciple/GoodCode/EmailService.java
@@ -0,0 +1,7 @@
+package solid_principles.SingleResponsibilityPrinciple.GoodCode;
+
+public class EmailService {
+    public void sendEmailNotification(){
+        System.out.println("Sending email notification for invoice");
+    }
+}
---
+++ solid_principles/SingleResponsibilityPrinciple/GoodCode/InvoiceRepository.java
@@ -0,0 +1,7 @@
+package solid_principles.SingleResponsibilityPrinciple.GoodCode;
+
+public class InvoiceRepository {
+    public void saveToDatabase(){
+        System.out.println("Saving invoice to Database");
+    }
+}
