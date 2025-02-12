478110d6765918555548cdd60bd5b1da52817354
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-13 01:15:09 +0530

    Dependancy Inversion Principle

--- solid_principles/DependancyInversionPrinciple/GoodCode/NotificationService.java
+++ solid_principles/DependancyInversionPrinciple/GoodCode/NotificationService.java
@@ -11,7 +11,8 @@ public class NotificationService {
     public void notify(String message){
         // incase if user implements new notification service for whatsApp, client don't need
         // to worry about if it has a send methods or not, because it has to since it'll implement a abstraction
-        // called NotificationChannel
+        // called NotificationChannel + high level module remains independant of specific low level details.
+        // here we can make changes to low level modules without affecting high level modules.
         notificationChannel.send(message);
     }
 
