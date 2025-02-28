d768860f4dc4dec169066fb32c35363e5dd580eb
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-02-28 23:19:10 +0530

    command method design pattern

---
+++ design_patterns/behavioural_design_patterns/CommandMethodDesignPattern/CommandMethodDesignPattern.java
@@ -0,0 +1,5 @@
+package design_patterns.behavioural_design_patterns.CommandMethodDesignPattern;
+
+public class CommandMethodDesignPattern {
+    
+}
--- design_patterns/behavioural_design_patterns/ObserverMethodDesignPattern/ObserverMethodDesignPattern.java
+++
@@ -1,5 +0,0 @@
-package design_patterns.behavioural_design_patterns.ObserverMethodDesignPattern;
-
-public class ObserverMethodDesignPattern {
-    
-}
