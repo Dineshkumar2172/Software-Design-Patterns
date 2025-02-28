4d602f94437e54f80375812376c48f35ad6005eb
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-01 00:03:41 +0530

    command pattern

--- design_patterns/behavioural_design_patterns/CommandMethodDesignPattern/CommandMethodDesignPattern.java
+++
@@ -1,5 +0,0 @@
-package design_patterns.behavioural_design_patterns.CommandMethodDesignPattern;
-
-public class CommandMethodDesignPattern {
-    
-}
---
+++ design_patterns/behavioural_design_patterns/CommandMethodDesignPattern/WithCommandPattern/main.java
@@ -0,0 +1,5 @@
+package design_patterns.behavioural_design_patterns.CommandMethodDesignPattern.WithCommandPattern;
+
+public class main {
+    
+}
