72c0b4a9498ea5178659f8ba9d6ccc2ec04ea2db
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-01 00:19:35 +0530

    command desing pattern implementation

--- design_patterns/behavioural_design_patterns/CommandMethodDesignPattern/WithCommandPattern/main.java
+++ design_patterns/behavioural_design_patterns/CommandMethodDesignPattern/WithCommandPattern/main.java
@@ -3,10 +3,10 @@ package design_patterns.behavioural_design_patterns.CommandMethodDesignPattern.W
 public class main {
     public static void main(String[] args) {
         TextEditor textEditor = new TextEditor();
-        BoldCommand boldCommand = new BoldCommand(textEditor);
-
+        
+        // Decoupling -> one button can do many types of actions, decoupled from the text
         Button button = new Button();
-        button.setCommand(boldCommand);
+        button.setCommand(new BoldCommand(textEditor));
         button.click();
     }
 }
