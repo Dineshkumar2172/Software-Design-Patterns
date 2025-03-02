c8d3cc1b1883c5aaf71b83e687d994eaadb78a9d
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-03-02 23:31:25 +0530

    template design pattern implementation

--- design_patterns/behavioural_design_patterns/TemplateMethodDesignPattern/WithTemplatePattern/TemplateMethodDesignPattern.java
+++ design_patterns/behavioural_design_patterns/TemplateMethodDesignPattern/WithTemplatePattern/TemplateMethodDesignPattern.java
@@ -1,5 +1,44 @@
 package design_patterns.behavioural_design_patterns.TemplateMethodDesignPattern.WithTemplatePattern;
 
-public class TemplateMethodDesignPattern {
+abstract class DataParser {
+    // Template Method - Defines the steps of the algorithm
+    public final void parse() {
+        openFile();
+        parseData();
+        closeFile();
+    }
+
+    protected void openFile() {
+        System.out.println("Opening File");
+    }
+
+    protected void closeFile() {
+        System.out.println("Closing File");
+    }
+
+    protected abstract void parseData();
+}
+
+class CSVParser extends DataParser {
+    protected void parseData() {
+        System.out.println("Parsing CSV Data");
+    }
     
+}
+
+class JSONParser extends DataParser {
+    protected void parseData() {
+        System.out.println("Parsing JSON Data");
+    }
+    
+}
+
+public class TemplateMethodDesignPattern {
+    public static void main(String[] args) {
+        DataParser jsonParser = new JSONParser();
+        DataParser csvParser = new CSVParser();
+
+        jsonParser.parse();
+        csvParser.parse();
+    }
 }
