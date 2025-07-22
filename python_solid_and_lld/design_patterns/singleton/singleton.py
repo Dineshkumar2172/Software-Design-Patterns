95b3ec3193482ef1a5840bd60df5bb0f5b49a936
Dineshkumar2172 <dinesh2000.data@gmail.com>
2025-07-22 22:50:57 +0530

    singleton

--- python_solid_and_lld/design_patterns/singleton/singleton.py
+++ python_solid_and_lld/design_patterns/singleton/singleton.py
@@ -1,13 +1,16 @@
 class Singleton:
-
+    # class variable to store single class instance
     _instance = None
 
+    # override the init method to control initialisation
     def __init__(self):
+        # raise an error to prevent controller initialization
         raise RuntimeError("Call instance() instead")
 
     @classmethod # staticmethod makes a method attached to class instead of instance
     def get_instance(cls): # for class methods, we pass the reference to class itself instead of instance reference self. we could use different terms as well, it's just for readability
-        if cls._instance == None:
+        if cls._instance == None: # NOTE: lazy initialisation
+            # create new instance of the class
             cls._instance = cls.__new__(cls)
         
         return cls._instance
