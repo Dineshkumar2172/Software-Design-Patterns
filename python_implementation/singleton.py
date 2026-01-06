fd6758918de35f28a4f8043417a5ca9ab1881780
Dineshkumar2172 <dinesh2000.data@gmail.com>
2026-01-06 20:29:44 +0530

    singleton design pattern

--- python_implementation/singleton.py
+++ python_implementation/singleton.py
@@ -3,6 +3,7 @@ import threading
 class SingletonClass:
     _instance = None
     _lock = threading.Lock()
+    _initialized = False
 
     def __new__(cls, *args, **kwargs):
         if cls._lock:
@@ -12,8 +13,26 @@ class SingletonClass:
 
         return cls._instance
 
+    def __init__(self, start):
+        # what if we wanna prevent initialization
+        # once first object is created. No further
+        # initialization should go through
+        if not self._initialized:
+            self.value = start
+            self._initialized = True
 
-a = SingletonClass()
-b = SingletonClass()
+    def __str__(self):
+        return str(self.value)
 
-print(a is b)
+    def increment(self):
+        self.value += 1
+
+a = SingletonClass(1)
+b = SingletonClass(10)
+
+b.increment()
+b.increment()
+b.increment()
+
+print(f"a is {a}")
+print(f"b is {b}")
